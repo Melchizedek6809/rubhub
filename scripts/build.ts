e23a0d3e2dd591a11dfc94a35231cd54634e1e0c
Ben (Desktop/Arch) <bennyschulenburg@gmx.de>
2025-12-10 20:22:47 +0100

    Use production version of lit in prod.

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,5 +3,5 @@ import { $ } from "bun";
 await $`rm -rf ./dist/`;
 
 await Promise.all([
-	$`bun build --minify ./frontend/app/app.html --outdir ./dist/ --public-path /dist/`,
+	$`NODE_ENV=production bun build --minify ./frontend/app/app.html --outdir ./dist/ --public-path /dist/`,
 ]);
